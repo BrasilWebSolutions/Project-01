d1137ef5cb94ea3ace638e3a37ff809ac27a0bf0
Fernando Britto <fernandobritto.web@gmail.com>
2019-02-03 10:15:47 -0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('webpack está funcionando');
+'use strict'
+var React = require('react');
+var Title = require('./app');
+
+ReactDOM.render(
+	React.createElement(Title),
+	document.querySelector('[data-js="app"]');
+)
