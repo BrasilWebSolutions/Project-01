af985b8df93438a17f453b09357222652cb0462c
Fernando Britto <me@fernandobritto.com.br>
2019-05-21 21:27:10 -0300

    Initial commit

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    title: "Contador",
-    home: Container(color: Colors.white,)
-  ));
+      title: "Contador",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Pessoa: 0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          Text("Pode entrar",
+              style:
+              TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30.0))
+        ],
+      ) //Coluna
+      )); //MaterialApp
 }
